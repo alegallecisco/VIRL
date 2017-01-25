3614ee7dca9db4b0735b35a19484585f822ec0ca
vTechie <alegalle@cisco.com>
2017-01-24 22:44:31 -0500

    clean-up of script -- added version reporting

--- srv_validate.sh
+++ srv_validate.sh
@@ -5,7 +5,7 @@ PROGNAME=$(basename $0)
 ## place them into a single file (SrvValTest.txt). This file can be collected and
 ## forwarded to VIRL support community for assistance.
 ## Validation script created by alejandro gallego (alegalle@cisco.com)
-## Last modified on Jan 23, 2017
+## Last modified on Jan 24, 2017
 
 TEMP_FILE=/tmp/${PROGNAME}.$$.$RANDOM
 
@@ -23,9 +23,7 @@ function _result
 {
 printf "%s\nResults printed to file \"$_out\" in
     \"virl user's home directory\"%s\n"
-sleep 2
-rm $_out >& /dev/null
-touch $_out
+sleep 1
 }
 
 ## Deployment type checks for VMware PCI devices
@@ -33,13 +31,28 @@ function _dtype
 {
 lspci |grep ' peripheral: VMware' > /dev/null
 if [[ $? -ne 0 ]] ; then
-    printf "%s\nInstallation Type: \"OTHER\"\n" >> $_out 2>&1
+    printf "%s\nInstallation Type: \"OTHER\"\n"
 else
-printf "%s\nInstallation Type: \"OVA\"\n" >> $_out 2>&1
+printf "%s\nInstallation Type: \"OVA\"\n"
 fi
-printf "%sVIRL Version:$ver\n"  >> $_out 2>&1
-printf "%sOS Info:
-$lver\n"  >> $_out 2>&1
+}
+
+## Checking installed version of typical packages
+function _verchk
+{
+echo "
+>>>Openstack / System Versions<<<
+"
+printf "%sVIRL Release:$ver\n"
+printf "%sOS Info:\n$lver\n"
+printf "%s\nNeutron: " && neutron --version
+printf "%sNova: " && nova --version
+printf "%sKeystone: " && keystone --version
+echo "
+>>>>VIRL Versions<<<<"
+printf "%s\n$(sudo salt-minion --versions)\n"
+printf "%s\nUWM Client: " && sudo virl_uwm_client version
+echo ""
 }
 
 ## Network information checks for configured interfaces, compares assigned MAC addr. to HW Mac addr.
@@ -52,22 +65,20 @@ ipadr=$(ip addr show dev $intf 2> /dev/null | awk '$1 == "inet" { sub("/.*", "",
 mac=$(ip link show $intf 2> /dev/null | awk '/ether/ {print $2}' )
 hwmac=$(cat /sys/class/net/$intf/address 2> /dev/null )
     printf "%s\n$intf CONFIGURED\n"
-    printf "%s\n$intf CONFIGURED\n" >> $_out 2>&1
-	printf "%s\nMAC: $mac\n" >> $_out 2>&1 && printf "HW:  $hwmac\n" >> $_out 2>&1
+	printf "%s\nMAC: $mac\n"
+	printf "%sHW:  $hwmac\n"
     ip link show $intf > /dev/null 2>&1
         if [ $? -ne 0 ] ; then
         printf ">>>>%sInterface $intf DOWN%s\n"
-        printf ">>>>%sInterface $intf DOWN%s\n" >> $_out 2>&1
         else
         printf "IP: $ipadr\n"
-        printf "IP: $ipadr\n" >> $_out 2>&1
         echo ""
         fi
 done
 
-printf "%s\nBridge Info: \n $lbrdg%s\n" >> $_out 2>&1
+printf "%s\nBridge Info: \n $lbrdg%s\n"
 vini=$(egrep '\bsalt_'\|'\bhost'\|'\bdomain'\|'\bpublic_'\|'\bStatic_'\|'\busing_'\|'\bl2_'\|'\bl3_'\|'\bdummy_'\|'\bvirl_'\|'\binternalnet_'\|'_nameserver' /etc/virl.ini)
-printf "%s\n>>> VIRL Config Summary <<<\n$vini" >> $_out 2>&1
+printf "%s\n>>> VIRL Config Summary <<<\n$vini"
 }
 
 ## Salt test will check configured salt servers, connectivity to configured salt servers, and license validation.
@@ -78,30 +89,24 @@ printf "%s\nCheckin Salt Configuration...%s\n"
 sleep 1
 printf "%s\nConfigured Salt masters:\n $mstr%s\n"
 printf "%s\nConfigured Salt ID:\n $lic%s\n"
-printf "%s\n\nSalt Masters\n $mstr %s\n"  >> $_out 2>&1
-printf "%s\nSalt ID\n $lic %s\n"  >> $_out 2>&1
 for srv in ${mstr//,/ }
     do
     idig=$(dig $srv | egrep -o '([0-9]+\.){3}[0-9]+' |head -1)
-    printf "%s\nTesting Connectivity to: [$srv $idig]%s"
-    printf "%s\n>>>> $srv :: $idig\n" >> $_out 2>&1 && nc -zv $srv 4505-4506 >> $_out 2>&1
+    printf "%s\nTesting Connectivity to: [$srv $idig]%s\n"
+    nc -zv $srv 4505-4506
     echo ""
     printf "%s\nChecking License....%s"
     printf "%s\nAuth test --> Salt Server [$srv]%s\n"
-    printf "%s\n>>>> $srv\n" >> $_out 2>&1 && sudo salt-call --master $srv -l debug test.ping >> $_out 2>&1
+    sudo salt-call --master $srv -l debug test.ping
     done
 
 printf "%s\nChecking hostname and network interfaces: %s\n"
 sleep 2
     for h in /etc/hostname /etc/hosts /etc/network/interfaces
     do
-        printf "%s\n>>> $h <<<%s\n" >> $_out 2>&1
-        cat $h >> $_out
+        printf "%s\n>>> $h <<<%s\n"
+        cat $h
     done
-sleep 2
-printf "%s\nResults printed to file \"$_out\" located in
-\"virl user's home directory\" Path is \"/home/virl\"%s\n"
-sleep 5
 }
 
 ## Global vars
@@ -113,7 +118,18 @@ lic=$(sudo salt-call --local grains.get id | egrep -v 'local:' )
 _out=~/SrvValTest.txt
 ###
 
+rm $_out >& /dev/null
+touch $_out
 _result
-_dtype
-_netint
-_saltst
+echo "Checking deployment type...."
+_dtype >> $_out 2>&1
+echo "Checking installed versions...."
+_verchk >> $_out 2>&1
+echo "Checking networking configuration...."
+_netint >> $_out 2>&1
+echo "Checking salt connectivity and license...."
+_saltst >> $_out 2>&1
+echo "
+DONE...."
+_result
+# sleep 5
