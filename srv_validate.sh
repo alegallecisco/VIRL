62c512d69c7f33a99c72cda887e743faa4dce0eb
vTechie <alegalle@cisco.com>
2017-03-07 15:01:45 -0500

    minor changes and clean up.

--- srv_validate.sh
+++ srv_validate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-PROGNAME=$(basename $0)
-
 ## This script will collect key parts of your VIRL server configuration settings and
 ## place them into a single file (SrvValTest.txt). This file can be collected and
 ## forwarded to VIRL support community for assistance.
 ## Validation script created by alejandro gallego (alegalle@cisco.com)
-## Last modified on Jan 27, 2017
+## Last modified on Mar 07, 2017
 ##
 ## Current version supports Openstack Kilo, Mitaka
 ##
@@ -15,16 +13,16 @@ PROGNAME=$(basename $0)
 
 function int_exit
 {
-        echo "${PROGNAME}: Aborted by user"
-        exit
+echo "${PROGNAME}: Aborted by user"
+exit
 }
 
 ## Results of commands in script are sent to text file. The text file
 ## will be found under the default username 'virl' home directory.
 function _result
 {
-printf "%s\nResults printed to file \"$_out\" in
-    \"virl user's home directory\"%s\n"
+printf "\nResults printed to text file \"%s\"
+    Located in virl user's home directory\n" $_out
 sleep 1
 }
 
@@ -42,6 +40,7 @@ fi
 ## Checking installed version of typical packages
 function _verchk
 {
+format="\n %-25s %10s\n"
 printf "%6s>>> Openstack / System Versions <<<\n"
 printf "%sVIRL Release:$ver\n" && sudo pip list | grep VIRL
 printf "%s\nOS Info:\n$lver\n\n"
@@ -51,11 +50,11 @@ printf "%17sNeutron: %s" && neutron --version
 printf "%20sNova: %s" && nova --version
 printf "%6s>>> Python Modules <<<\n"
 declare iver=($(sudo pip list | egrep '\bautonetkit'\|'\bvirl-'))
- echo "              AutoNetkit:  ${iver[1]}"
- echo "        AutoNetkit Cisco:  ${iver[3]}"
- echo "       Topology Vis Eng.:  ${iver[5]}"
- echo "Live Net Collection Eng.:  ${iver[7]}"
- echo ""
+    echo "              AutoNetkit:  ${iver[1]}"
+    echo "        AutoNetkit Cisco:  ${iver[3]}"
+    echo "       Topology Vis Eng.:  ${iver[5]}"
+    echo "Live Net Collection Eng.:  ${iver[7]}"
+    echo ""
 printf "%6s>>> >Salt Version <<<<\n"
 printf "%s$(sudo salt-minion --versions)\n"
 echo ""
@@ -68,7 +67,7 @@ grep -i xenial /etc/os-release > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     _kostack >> $_out 2>&1
     else
-_ostack >> $_out 2>&1
+    _ostack >> $_out 2>&1
 fi
 }
 
@@ -76,22 +75,22 @@ fi
 function _ostack
 {
 printf "%6s>>> Openstack Info / Stats <<<<"
-printf "\n%5sVIRL Host %s\n" && openstack host show virl
-printf "\n%5sVIRL Images %s\n" && openstack image list
-printf "\n%5sOpenStack Neutron%s\n$ntrn\n"
-printf "\n%5sOpenStack Nova %s\n$nva\n"
-printf "\n%5sVIRL Hypervisor %s\n" && openstack hypervisor stats show
-printf "\n%5sOpenStack Services %s\n" && openstack service list --long
+printf "\n%5sVIRL Host \n%s" && openstack host show virl
+printf "\n%5sVIRL Images \n%s" && openstack image list
+printf "%23s\n%s\n" "OpenStack Neutron" "$ntrn"
+printf "%20s\n%s\n" "OpenStack Nova" "$nva"
+printf "\n%5sVIRL Hypervisor \n%s" && openstack hypervisor stats show
+printf "\n%5sOpenStack Services \n%s" && openstack service list --long
 }
 
 function _kostack
 {
 printf "%6s>>> Openstack Info / Stats <<<<"
-printf "\n%5sVIRL Host %s\n" && nova host-list
-printf "\n%5sVIRL Images %s\n" && nova image-list
-printf "\n%5sOpenStack Neutron%s\n$ntrn\n"
-printf "\n%5sOpenStack Nova %s\n$nva\n"
-printf "\n%5sVIRL Hypervisor %s\n" && nova hypervisor-stats
+printf "\n%5sVIRL Host \n%s" && nova host-list
+printf "\n%5sVIRL Images \n%s" && nova image-list
+printf "%23s\n%s\n" "OpenStack Neutron" "$ntrn"
+printf "%20s\n%s\n" "OpenStack Nova" "$nva"
+printf "\n%5sVIRL Hypervisor \n%s" && nova hypervisor-stats
 }
 
 
@@ -105,50 +104,52 @@ do
 ipadr=$(ip addr show dev $intf 2> /dev/null | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }' )
 mac=$(ip link show $intf 2> /dev/null | awk '/ether/ {print $2}' )
 hwmac=$(cat /sys/class/net/$intf/address 2> /dev/null )
-    printf "%s$intf CONFIGURED"
-	printf "%s\nMAC: $mac\n"
-	printf "%sHW:  $hwmac\n"
+    printf "%s CONFIGURED\n" "$intf"
+	printf "MAC: %s\n" "$mac"
+	printf "HW:  %s\n" "$hwmac"
     ip link show $intf > /dev/null 2>&1
         if [ $? -ne 0 ] ; then
-        printf ">>> %sInterface $intf DOWN%s\n"
+        printf ">>> Interface %s DOWN\n" "$intf"
         else
-        printf "IP: $ipadr\n"
+        printf "IP: %s\n" "$ipadr"
         echo ""
         fi
 done
 
-printf "%s\nBridge Info: \n $lbrdg%s\n"
+## Print summary
+printf "\nBridge Info: \n %s\n" "$lbrdg"
 vini=$(egrep '\bsalt_'\|'\bhost'\|'\bdomain'\|'\bpublic_'\|'\bStatic_'\|'\busing_'\|'\bl2_'\|'\bl3_'\|'\bdummy_'\|'\bvirl_'\|'\binternalnet_'\|'_nameserver' /etc/virl.ini)
-printf "\n%6s>>> VIRL Config Summary <<<\n$vini"
+printf "\n>>> VIRL Config Summary <<<\n%s" "$vini"
+printf "\nChecking hostname and network interfaces...\n"
+sleep 2
+    for h in /etc/hostname /etc/hosts /etc/network/interfaces
+    do
+        printf "\n>>> %s <<<\n" "$h"
+        cat $h
+    done
 }
 
 ## Salt test will check configured salt servers, connectivity to configured salt servers, and license validation.
 ## License validation only checks to see if configured license is accepted not expiry or syntax.
 function _saltst
 {
-printf "%s\nCheckin Salt Configuration...%s\n"
+printf "\n%s\n" "Checking Salt Configuration..."
 sleep 1
-printf "%s\nConfigured Salt masters:\n $mstr%s\n"
-printf "%s\nConfigured Salt ID:\n $lic%s\n"
+printf "\nConfigured Salt masters:\n %s\n" "$mstr"
+printf "\nConfigured Salt ID:\n %s\n" "$lic"
 for srv in ${mstr//,/ }
     do
     idig=$(dig $srv | egrep -o '([0-9]+\.){3}[0-9]+' |head -1)
-    printf "%s\nTesting Connectivity to: [$srv $idig]%s\n"
+    printf "\nTesting Connectivity to: [%3s %s]\n" "$srv" "$idig"
     nc -zv $srv 4505-4506
     echo ""
-    printf "%s\nChecking License....%s"
-    printf "%s\nAuth test --> Salt Server [$srv]%s\n"
+    printf "\nChecking License...."
+    printf "\nAuth test --> Salt Server [%s]\n" "$srv"
     sudo salt-call --master $srv -l debug test.ping
     done
-
-printf "%s\nChecking hostname and network interfaces: %s\n"
-sleep 2
-    for h in /etc/hostname /etc/hosts /etc/network/interfaces
-    do
-        printf "%s\n>>> $h <<<%s\n"
-        cat $h
-    done
 }
+
+PROGNAME=$(basename $0)
 
 ## Global vars
 ntrn=$(neutron agent-list)
